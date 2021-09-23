b4921daaa3157932fd87f3411f5b038aa492ed73
Anjali <anjalikumari972001@gmail.com>
2021-09-23 21:32:19 +0530

    Initial commit

---
+++ src/com/Anjali/Main.java
@@ -0,0 +1,14 @@
+package com.Anjali;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 1;
+        do{
+            System.out.println("Hello World");
+
+
+        }while(n != 1);
+	// do while loop will run atleast once even if given condition is not true. but not in case of other loop
+    }
+}
